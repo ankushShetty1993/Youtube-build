e0d8b422aea2c417ca3dd370a2a4ef97067f8ca3
Ankush <nitin@codewave.in>
2020-07-19 20:36:47 +0530

    Constants removed

---
+++ Youtube-build/ConstantsTemplate.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Youtube-build
+//
+//  Created by Admin on 7/19/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+//Uncomment the code and add your keys to it.
+
+import Foundation
+
+//
+//struct Constants {
+//
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//}
